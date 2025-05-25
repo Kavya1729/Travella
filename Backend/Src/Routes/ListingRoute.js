a0107813967fbf83637212f48e1ed40ae755fd36
Kavya Mittal <mister1729x@gmail.com>
2025-05-25 23:55:33 +0530

    making models for listings and controller

--- Backend/Src/Routes/ListingRoute.js
+++ Backend/Src/Routes/ListingRoute.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const router = express.Router();
-// const { allListings, getListing, createListing, updateListing, deleteListing } = require('../Controllers/ListingController');
+const { allListings, getListing, createListing, updateListing, deleteListing } = require('../Controllers/ListingController');
 
-// router.get('/allListings', allListings);
-// router.get('/:id',getListing);
-// router.post('/create',createListing);
-// router.put('/update/:id',updateListing);
-// router.delete('/delete/:id',deleteListing);
+router.get('/', allListings);
+router.get('/:id',getListing);  //check why not working
+router.post('/create',createListing);
+router.put('/update/:id',updateListing);
+router.delete('/delete/:id',deleteListing);
+
+module.exports = router;
